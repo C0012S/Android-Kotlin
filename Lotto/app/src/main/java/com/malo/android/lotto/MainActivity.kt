25b6ff726bd0f2db8d6f9d89f7ac7378392ddc21
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-18 19:53:43 +0900

    코틀린 코드로 속성 변경하기 - 버튼이 눌렸는지 로그로 확인하기

--- Lotto/app/src/main/java/com/malo/android/lotto/MainActivity.kt
+++ Lotto/app/src/main/java/com/malo/android/lotto/MainActivity.kt
@@ -2,10 +2,16 @@ package com.malo.android.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.runButton).setOnClickListener {
+            Log.d("로또 앱", "버튼 누름")
+        }
     }
 }
