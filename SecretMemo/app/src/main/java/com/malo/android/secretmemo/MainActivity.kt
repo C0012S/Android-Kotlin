486221df2a9e66a6ab0d68549a54babf1c0a37d4
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-24 21:18:22 +0900

    비밀번호 체크하기

--- SecretMemo/app/src/main/java/com/malo/android/secretmemo/MainActivity.kt
+++ SecretMemo/app/src/main/java/com/malo/android/secretmemo/MainActivity.kt
@@ -2,10 +2,26 @@ package com.malo.android.secretmemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editTextNumberPassword)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val passwordText = editText.text.toString()
+
+            if (passwordText == "12345678") {
+                Log.d("Secret Memo", "통과.")
+            }
+            else {
+                Log.d("Secret Memo", "틀렸어요.")
+            }
+        }
     }
 }
