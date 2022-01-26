7d93992063414b7a54d8444321831ff8a0c59afb
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-26 23:36:09 +0900

    이메일과 비밀번호가 맞을 경우 Activity 전환하기

--- SecretMemo/app/src/main/java/com/malo/android/secretmemo/MainActivity.kt
+++ SecretMemo/app/src/main/java/com/malo/android/secretmemo/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.malo.android.secretmemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         val editEmail = findViewById<EditText>(R.id.editEmail)
         val editPassword = findViewById<EditText>(R.id.editPassword)
 
+        val intent = Intent(this, MemoActivity::class.java)
+        val enter_text = findViewById<TextView>(R.id.text)
+
         findViewById<Button>(R.id.button).setOnClickListener {
             val emailText = editEmail.text.toString()
             val passwordText = editPassword.text.toString()
@@ -21,13 +26,18 @@ class MainActivity : AppCompatActivity() {
             if (emailText == "aaa@gmail.com") {
                 if (passwordText == "12345678") { // 이메일과 비밀번호가 맞을 경우
                     Log.d("Secret Memo", "통과.")
+                    enter_text.text = "이메일과 비밀번호가 맞았어요."
+                    startActivity(intent)
+                    this.finish()
                 }
                 else { // 이메일이 맞지만 비밀번호가 틀렸을 경우
                     Log.d("Secret Memo", "비밀번호가 틀렸어요.")
+                    enter_text.text = "비밀번호가 틀렸어요. 다시 입력해 주세요."
                 }
             }
             else { // 이메일이 틀렸을 경우
                 Log.d("Secret Memo", "이메일이 틀렸어요.")
+                enter_text.text = "이메일이 틀렸어요."
             }
         }
     }
