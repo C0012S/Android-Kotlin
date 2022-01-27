60e8225e6c72c3aa9538184f06ae447570502a5e
C0012S <qkrtkd1002@duksung.ac.kr>
2022-01-27 22:41:55 +0900

    입력한 메모를 공유하기

--- SecretMemo/app/src/main/java/com/malo/android/secretmemo/MemoActivity.kt
+++ SecretMemo/app/src/main/java/com/malo/android/secretmemo/MemoActivity.kt
@@ -1,11 +1,28 @@
 package com.malo.android.secretmemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo)
+
+        val shareButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val memo = findViewById<EditText>(R.id.editMemo)
+
+        shareButton.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, memo.text.toString())
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
     }
 }
